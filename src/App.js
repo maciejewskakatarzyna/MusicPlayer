01c51ceeb473c07f65d9d1aa961cf183718c80c6
kasiamaciejewska <maciejewska.kontakt@gmail.com>
2021-07-08 11:10:47 +0200

    props

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import "./styles.css";
 
-function Heading() {
-    const showHello  = 1 + 1 === 3 || 2 === 2
-    return <h1>{showHello && "hello"}</h1>;
+function Heading(props) {
+const title = props.title
+    return <h1>{title}</h1>;
 }
-function SongPlayer() {
-    const showControls = false || true;
-    const audioUrl = "https://examples.devmastery.pl/assets/audio/deadfro5h.mp3"
+function SongPlayer(props) {
+    const showControls = props.showControls
+    const audioUrl = props.audioUrl
     return (
         <section>
-            <Heading />
+            <Heading title={"Music Player"}/>
             <audio controls={showControls}>
                 <source src={audioUrl} />
             </audio>
@@ -17,25 +17,12 @@ function SongPlayer() {
     );
 }
 
-function getStatusMessage(isLoading, hasErrors) {
-    let message = null;
-    if (isLoading) {
-        message = "Loading..."
-    }
-    if (hasErrors) {
-        message = "Error occured..."
-    }
-    return message
-}
 
 export default function App() {
-    const isLoading = false
-    const hasErrors = false
-    const showPlayer = !isLoading && !hasErrors
-    const statusMessage = getStatusMessage(isLoading, hasErrors)
+
     return (
         <div className="App">
-            {showPlayer ? <SongPlayer /> : statusMessage}
+            <SongPlayer showControls audioUrl={"https://examples.devmastery.pl/assets/audio/deadfro5h.mp3"}/>
         </div>
     );
 }
