93c99a2e81d7e3a7936e4e0a8edf41d3bf18384f
kasiamaciejewska <maciejewska.kontakt@gmail.com>
2021-07-08 10:59:52 +0200

    status messages

--- src/App.js
+++ src/App.js
@@ -1,22 +1,41 @@
 import "./styles.css";
 
 function Heading() {
-    return <h1>Music Player</h1>;
+    const showHello  = 1 + 1 === 3 || 2 === 2
+    return <h1>{showHello && "hello"}</h1>;
 }
 function SongPlayer() {
+    const showControls = false || true;
+    const audioUrl = "https://examples.devmastery.pl/assets/audio/deadfro5h.mp3"
     return (
         <section>
             <Heading />
-            <audio controls>
-                <source src="https://examples.devmastery.pl/assets/audio/deadfro5h.mp3" />
+            <audio controls={showControls}>
+                <source src={audioUrl} />
             </audio>
         </section>
     );
 }
+
+function getStatusMessage(isLoading, hasErrors) {
+    let message = null;
+    if (isLoading) {
+        message = "Loading..."
+    }
+    if (hasErrors) {
+        message = "Error occured..."
+    }
+    return message
+}
+
 export default function App() {
+    const isLoading = false
+    const hasErrors = false
+    const showPlayer = !isLoading && !hasErrors
+    const statusMessage = getStatusMessage(isLoading, hasErrors)
     return (
         <div className="App">
-            <SongPlayer />
+            {showPlayer ? <SongPlayer /> : statusMessage}
         </div>
     );
 }
