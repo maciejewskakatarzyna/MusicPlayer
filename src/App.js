e02008c32ef8ac8b67a832a396f72aa0d227211b
kasiamaciejewska <maciejewska.kontakt@gmail.com>
2021-07-09 14:48:01 +0200

    songs array

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function SongPlayer({ showControls = true, song }) {
         <section>
             <Heading title={"Music Player"}/>
             <img width="250px" height="250px" src={coverUrl} alt="Song cover" />
-            <audio controls={showControls}>
+            <audio key={audioUrl} controls={showControls}>
                 <source src={audioUrl} />
             </audio>
         </section>
@@ -18,12 +18,27 @@ function SongPlayer({ showControls = true, song }) {
 
 
 export default function App() {
-const currentSong = {
-    audioUrl:"https://examples.devmastery.pl/assets/audio/deadfro5h.mp3",
-    coverUrl:"https://examples.devmastery.pl/assets/audio/deadfro5h.jpg",
-    title: "Deadfro5h",
-    artist: "starfrosh"
-}
+     const songs = [
+         {
+             audioUrl: "https://examples.devmastery.pl/assets/audio/deadfro5h.mp3",
+             coverUrl: "https://examples.devmastery.pl/assets/audio/deadfro5h.jpg",
+             title: "Deadfro5h",
+             artist: "starfrosh"
+         },
+         {
+             audioUrl: "https://examples.devmastery.pl/assets/audio/majesty.mp3",
+             coverUrl: "https://examples.devmastery.pl/assets/audio/majesty.jpg",
+             title: "Majesty (Original Mix)",
+             artist: "Ryan Craig Martin"
+         },
+         {
+             audioUrl: "https://examples.devmastery.pl/assets/audio/runs.mp3",
+             coverUrl: "https://examples.devmastery.pl/assets/audio/runs.jpg",
+             title: "Runs",
+             artist: "Wowa"
+         }
+     ]
+const currentSong = songs[1];
     return (
         <div className="App">
             <SongPlayer song={currentSong}/>
